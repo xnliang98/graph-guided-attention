9a71376727e3cc6c922e95156a1baed63cf33165
xnliang <xnliang19@163.com>
2019-12-12 20:00:16 +0800

    conv l2

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 
 python train.py --id 03 \
      --seed 0 \
-     --hidden_dim 300 \
+     --hidden_dim 200 \
      --rnn_layer 2 \
-     --lr 1.0 \
+     --lr 0.3 \
      --num_epoch 100 \
      --prune -1 \
-     --lr_decay 0.8 \
-     --gcn_layer 4
+     --lr_decay 0.95 \
+     --gcn_layer 3 \
+     --conv_l2 0.001
 
 
